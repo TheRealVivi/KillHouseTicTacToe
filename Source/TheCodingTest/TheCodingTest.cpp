3f963d1082687ba3eb59fa2d25ddf6c78540ff95
Kyle O'Dell <kodell135@gmail.com>
2021-05-30 21:57:27 -0700

    Added fbx model for main character

---
+++ Source/TheCodingTest/TheCodingTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheCodingTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheCodingTest, "TheCodingTest" );

afe08c9d1cf8728dbb7e99278939b2c6baff1289
Kyle O'Dell <kodell135@gmail.com>
2021-05-30 23:50:13 -0700

    Wrote MainPlayer .cpp/.hf files

--- Source/TheCodingTest/MainPlayer.cpp
+++ Source/TheCodingTest/MainPlayer.cpp
@@ -2,12 +2,75 @@
 
 
 #include "MainPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
 
 // Sets default values
 AMainPlayer::AMainPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	CameraBoomArmLength = 600.f;
+
+	// Creating Camera Boom
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = CameraBoomArmLength;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	// Setting default capsule component size
+	GetCapsuleComponent()->SetCapsuleSize(34.13f, 91.2f);
+
+	// Creating Camera
+	FollowingCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowingCamera"));
+	FollowingCamera->SetupAttachment(GetCameraBoom(), USpringArmComponent::SocketName);
+
+	// Attach the camera to the end of the boom and let the boom adjust to match
+	// the controller orientation
+	FollowingCamera->bUsePawnControlRotation = false;
+
+	// Don't rotate when the player rotates (Camera rotates only)
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	// Default values for bools;
+	bShiftKeyDown = false;
+	bEDown = false;
+	bLMBDown = false;
+	bRMBDown = false;
+	bWeaponEquipped = false;
+	bReplicates = true;
+	bAlwaysRelevant = true;
+
+	// Defaults for character movement controller
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+	GetCharacterMovement()->JumpZVelocity = 650.f;
+	GetCharacterMovement()->AirControl = .2f;
+
+	// Default stats for the player
+	Name = FString("Player");
+	MaxHealth = 100.f;
+	Health = MaxHealth;
+	Exp = 0.f;
+	MaxExp = 100.f;
+	PointsEarned = 0;
+	Stamina = 100.f;
+	MaxStamina = 150.f;
+	StaminaDrainRate = 15.f;
+	MinSprintStamina = 50.f;
+	WalkingSpeed = 650.f;
+	RunningSpeed = 950.f;
+	Level = 1;
+	MaxLevel = 99;
+	
+	MaxHealthIncrease = 20.f;
 
 }
 
@@ -29,6 +92,330 @@ void AMainPlayer::Tick(float DeltaTime)
 void AMainPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AMainPlayer::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AMainPlayer::MoveRight);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AMainPlayer::Jump);
+	PlayerInputComponent->BindAction(TEXT("MoveRight"), EInputEvent::IE_Released, this, &AMainPlayer::StopJumping);
+
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AMainPlayer::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AMainPlayer::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Pressed, this, &AMainPlayer::ShiftKeyDown);
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Released, this, &AMainPlayer::ShiftKeyUp);
+
+	PlayerInputComponent->BindAction(TEXT("Interact"), EInputEvent::IE_Pressed, this, &AMainPlayer::EKeyDown);
+	PlayerInputComponent->BindAction(TEXT("Interact"), EInputEvent::IE_Released, this, &AMainPlayer::EKeyUp);
+
+	PlayerInputComponent->BindAction(TEXT("Fire"), EInputEvent::IE_Pressed, this, &AMainPlayer::LMBDown);
+	PlayerInputComponent->BindAction(TEXT("Fire"), EInputEvent::IE_Released, this, &AMainPlayer::LMBUp);
 }
 
+void AMainPlayer::MoveForward(float Input) 
+{
+	if (Controller != nullptr && Input != 0.0f) 
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Input);
+	}
+}
+
+void AMainPlayer::MoveRight(float Input) 
+{
+	if (Controller != nullptr && Input != 0.f) 
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Input);
+	}
+}
+
+void AMainPlayer::ShiftKeyDown() 
+{
+	if (IsLocallyControlled()) // Client
+	{
+		ShiftKeyDownServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bShiftKeyDown = true;
+}
+
+// Server RPC for client to press shift key
+void AMainPlayer::ShiftKeyDownServer_Implementation() 
+{
+	bShiftKeyDown = true;
+}
+
+void AMainPlayer::ShiftKeyUp() 
+{
+	if (IsLocallyControlled()) // Client
+	{
+		ShiftKeyUpServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bShiftKeyDown = false;
+}
+
+// Server RPC for client to press shift key
+void AMainPlayer::ShiftKeyUpServer_Implementation()
+{
+	bShiftKeyDown = false;
+}
+
+void AMainPlayer::EKeyDown()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		EKeyDownServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bEDown = true;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::EKeyDownServer_Implementation()
+{
+	bEDown = true;
+}
+
+void AMainPlayer::EKeyUp()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		EKeyUpServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bEDown = false;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::EKeyUpServer_Implementation()
+{
+	bEDown = false;
+}
+
+void AMainPlayer::LMBDown()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		LMBDownServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bLMBDown = true;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::LMBDownServer_Implementation()
+{
+	bLMBDown = true;
+}
+
+void AMainPlayer::LMBUp()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		LMBUpServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bLMBDown = false;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::LMBUpServer_Implementation()
+{
+	bLMBDown = false;
+}
+
+void AMainPlayer::RMBDown()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		RMBDownServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bRMBDown = true;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::RMBDownServer_Implementation()
+{
+	bRMBDown = true;
+}
+
+void AMainPlayer::RMBUp()
+{
+	if (IsLocallyControlled()) // Client
+	{
+		RMBUpServer(); // RPC
+	}
+
+	if (GetLocalRole() == ROLE_Authority) // Server
+		bRMBDown = false;
+}
+
+// Server RPC for client to press E key
+void AMainPlayer::RMBUpServer_Implementation()
+{
+	bRMBDown = false;
+}
+
+void AMainPlayer::Die() 
+{
+	Health = MaxHealth;
+}
+
+void AMainPlayer::GetLifetimeReplicatedProps(TArray <FLifetimeProperty>& OutLifetimeProps) const 
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate the following variables
+	DOREPLIFETIME(AMainPlayer, Name);
+	DOREPLIFETIME(AMainPlayer, Health);
+	DOREPLIFETIME(AMainPlayer, Stamina);
+	DOREPLIFETIME(AMainPlayer, Level);
+	DOREPLIFETIME(AMainPlayer, Exp);
+	DOREPLIFETIME(AMainPlayer, MaxExp);
+	DOREPLIFETIME(AMainPlayer, PointsEarned);
+}
+
+void AMainPlayer::OnNameUpdate() 
+{
+	if (IsLocallyControlled()) 
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority) 
+	{
+
+	}
+}
+
+void AMainPlayer::OnHealthUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnStaminaUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnExpUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnMaxExpUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnLevelUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnPointsEarnedUpdate()
+{
+	if (IsLocallyControlled())
+	{
+
+	}
+
+	if (GetLocalRole() == ROLE_Authority)
+	{
+
+	}
+}
+
+void AMainPlayer::OnRep_Name()
+{
+	OnNameUpdate();
+}
+
+void AMainPlayer::OnRep_Health() 
+{
+	OnHealthUpdate();
+}
+
+void AMainPlayer::OnRep_Stamina()
+{
+	OnStaminaUpdate();
+}
+
+void AMainPlayer::OnRep_Exp()
+{
+	OnExpUpdate();
+}
+
+void AMainPlayer::OnRep_MaxExp()
+{
+	OnMaxExpUpdate();
+}
+
+void AMainPlayer::OnRep_Level()
+{
+	OnLevelUpdate();
+}
+
+void AMainPlayer::OnRep_PointsEarned()
+{
+	OnPointsEarnedUpdate();
+}
--- Source/TheCodingTest/MainPlayer.h
+++ Source/TheCodingTest/MainPlayer.h
@@ -15,9 +15,84 @@ public:
 	// Sets default values for this character's properties
 	AMainPlayer();
 
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowingCamera;
+
+	// Stats
+	UPROPERTY(ReplicatedUsing = OnRep_Name, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	FString Name;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Health, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	float Health;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Player Stats")
+	float MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stat Boosts")
+	float MaxHealthIncrease;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Stamina, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	float Stamina;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Player Stats")
+	float MaxStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float StaminaDrainRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MinSprintStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float WalkingSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float RunningSpeed;
+
+	UPROPERTY(ReplicatedUsing = OnRep_PointsEarned, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	int32 PointsEarned;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Exp, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	float Exp;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxExp, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	float MaxExp;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Level, EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+	int32 Level;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Stats")
+	int32 MaxLevel;
+
+	UPROPERTY(EditAnywhere, Category = Camera)
+	float CameraBoomArmLength; // length of camera boom
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Start Point")
+	FVector StartPoint; // Where player spawns.
+
+	bool bShiftKeyDown; // is shift key down
+	bool bEDown; // is E key down
+	bool bLMBDown; // is lmb down
+	bool bRMBDown; // is rmb down
+	bool bWeaponEquipped; // Does player have a weapon equipped
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void OnHealthUpdate();
+	void OnStaminaUpdate();
+	void OnExpUpdate();
+	void OnMaxExpUpdate();
+	void OnLevelUpdate();
+	void OnNameUpdate();
+	void OnPointsEarnedUpdate();
 
 public:	
 	// Called every frame
@@ -25,5 +100,68 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/**
+	* For player input controller
+	*/
+	void MoveForward(float Input);
+	void MoveRight(float Input);
+
+	void ShiftKeyDown();
+	void ShiftKeyUp();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger SHIFT key being pressed
+	void ShiftKeyDownServer();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger SHIFT key being released
+	void ShiftKeyUpServer();
+
+	void EKeyDown();
+	void EKeyUp();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger E key being pressed
+	void EKeyDownServer();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger E key being released
+	void EKeyUpServer();
+
+	void LMBDown();
+	void LMBUp();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger LMB key being pressed
+	void LMBDownServer();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger LMB key being released
+	void LMBUpServer();
+
+	void RMBDown();
+	void RMBUp();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger RMB key being pressed
+	void RMBDownServer();
+	UFUNCTION(Server, Reliable) // Server RPC to trigger RMB key being released
+	void RMBUpServer();
+
+	void Die();
+	void RespawnPlayer();
+
+
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowingCamera() const { return FollowingCamera; }
+
+	UFUNCTION()
+	void OnRep_Name();
+
+	UFUNCTION()
+	void OnRep_Health();
+
+	UFUNCTION()
+	void OnRep_Stamina();
+
+	UFUNCTION()
+	void OnRep_Exp();
+
+	UFUNCTION()
+	void OnRep_MaxExp();
+
+	UFUNCTION()
+	void OnRep_Level();
+
+	UFUNCTION()
+	void OnRep_PointsEarned();
+
 
 };
